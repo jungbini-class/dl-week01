9f4b6d1afb509de806d7ed040bf223d3ee8d52be
jungbini <jungbini@gmail.com>
2025-12-29 15:18:23 +0900

    Updated Changes

--- week01_ex01.py
+++ week01_ex01.py
@@ -4,3 +4,5 @@ from sklearn.linear_model import LinearRegression
 
 # 데이터를 다운로드하고 준비합니다.
 data_root = "https//github.com/ageron/data/raw/main/"
+lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
+X = lifesat[["GDP per capita (USD)"]].values
